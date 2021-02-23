4e393d3836a646cadf8d78ba044a30599871bd84
Guilherme Francisco <guilherme.francis2017@gmail.com>
2021-02-22 23:02:56 -0300

    Criação do controller responsável pela manipulação da autorização utilizando o serviço criado.

---
+++ back-end/userManager.Application/Controllers/AuthorizationController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using userManager.Domain.DTOs.Usuario;
+using userManager.Domain.Interfaces;
+
+namespace userManager.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+        private readonly IServiceAuthorization _serviceAuthorization;
+        public AuthorizationController(IServiceAuthorization serviceAuthorization) => _serviceAuthorization = serviceAuthorization;
+
+        [HttpPost("entrar")]
+        public async Task<ActionResult> Login(LoginUserDTO loginUser)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState.Values.SelectMany(e => e.Errors));
+
+            var result = await _serviceAuthorization.Entrar(loginUser);
+
+            if (result.Succeeded)
+            {
+                //gerar token e retornar
+                return Ok(new
+                {
+                    success = true,
+                    data = "token"
+                });
+            }
+
+            return BadRequest("Usuário ou senha inválidos");
+        }
+
+        [HttpPost("registrar")]
+        public async Task<ActionResult> Cadastrar(RegisterUserDTO registerUser)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState.Values.SelectMany(e => e.Errors));
+
+            var result = await _serviceAuthorization.Registrar(registerUser);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            var x = await _serviceAuthorization.Entrar(new LoginUserDTO() { Email = registerUser.Email, Senha = registerUser.Senha });
+
+            //gerar token e retornar
+            return Ok(new
+            {
+                success = true,
+                data = "token"
+            });
+        }
+
+    }
+}
